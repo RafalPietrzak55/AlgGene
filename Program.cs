1e13b9c47f3030f5798964a5d25c709cce24f114
RafalPietrzak55 <115399790+RafalPietrzak55@users.noreply.github.com>
2025-04-08 14:37:08 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgGene
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+           Random random = new Random();
+
+            const int PopulationSize = 9;
+            const int ChromosomeBits = 3;
+            const int Iterations = 20;
+            const int Tournaments = 2;
+            const double MinValue = 0.0;
+            const double MaxValue = 100.0;    
+
+
+        }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
