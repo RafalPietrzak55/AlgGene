66cf909368e27b38c5ee78c626e2c0383b8f5444
RafalPietrzak55 <115399790+RafalPietrzak55@users.noreply.github.com>
2025-04-12 09:15:01 +0200

    2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 class GeneticAlgorithm
 {
@@ -14,6 +15,7 @@ class GeneticAlgorithm
 
     static void Main(string[] args)
     {
+<<<<<<< HEAD
         List<int[]> population = InitializePopulation();
 
         for (int iteration = 0; iteration <= Iterations; iteration++)
@@ -22,6 +24,11 @@ class GeneticAlgorithm
             double bestFitness = fitnessValues.Max();
             double averageFitness = fitnessValues.Average();
             Console.WriteLine($"Iteracja {iteration}: Najlepsza: {bestFitness:F4}, Średnia: {averageFitness:F4}");
+=======
+        
+        
+      
+>>>>>>> dac8696930e02caf5f5c1bae4678ded3d4deddc9
 
             if (iteration == Iterations) break;
 
@@ -34,13 +41,108 @@ class GeneticAlgorithm
                 newPopulation.Add(mutatedChild);
             }
 
+<<<<<<< HEAD
             int bestIndex = fitnessValues.IndexOf(bestFitness);
             newPopulation.Add(population[bestIndex]);
+=======
+       
+
+        static void Main(string[] args)
+        
+        
+        {
+            List<int[]> population = InitializePopulation();
+
+            for (int iteration = 0; iteration <= Iterations; iteration++)
+
+            {
+                var fitnessValues = population.Select(DecodeAndEvaluate).ToList();
+
+                double bestFitness = fitnessValues.Max();
+                double averageFitness = fitnessValues.Average();
+
+                Console.WriteLine($"Iteracja {iteration}: Najlepsza = {bestFitness}, Srednia = {averageFitness}");
+
+                if (iteration == Iterations) break;
+
+
+                List<int[]> newPopulation = new List<int[]>();
+
+
+
+            }
+
+
+
+            static List<int> InitializePopulation()
+            {
+                List<int> population = new List<int>();
+                for (int i = 0; i < PopulationSize; i++)
+                {
+                    int[] chromosome = new int[ChromosomeBits * 2];
+                    for (int j = 0; j < chromosome.Length; j++)
+                        chromosome[j] = Random.Next(2);
+                    population.Add(chromosome);
+                }
+                return population;
+            }   
+           
+            
+
+
+
+
+            static double Decode(int[] bits)
+            {
+                int vaule = 0; 
+                for (int i = 0; i < bits.Length; i++)
+                {
+                    vaule += bits[i] * (1 << (bits.Length - i - 1));
+
+                    double range = MaxValue - MinValue; 
+                    return MinValue + vaule * range / (Math.Pow(2, ChromosomeBits) - 1);
+                }
+            }
+
+
+
+
+            static double FitnessFunction(double x1, double x2)
+            {
+                return Math.Sin(0.05 * x1) + Math.Sin(0.05 * x2) + 0.4 * Math.Sin(0.15 * x1) * Math.Sin(0.15 * x2);
+
+            }
+
+
+            static double DecodeAndEvaluate(int[] chromosome)
+            {
+                double x1 = Decode(chromosome.Take(ChromosomeBits).ToArray());
+                double x2 = Decode(chromosome.Skip(ChromosomeBits).Take(ChromosomeBits).ToArray()); 
+
+                return FitnessFunction(x1, x2);
+            }
+
+
+
+
+          
+
+
+
+
+
+        }
+
+
+
+            
+>>>>>>> dac8696930e02caf5f5c1bae4678ded3d4deddc9
 
             population = newPopulation;
         }
     }
 
+<<<<<<< HEAD
     static List<int[]> InitializePopulation()
     {
         List<int[]> population = new List<int[]>();
@@ -96,3 +198,8 @@ class GeneticAlgorithm
         return mutatedChromosome;
     }
 }
+=======
+
+
+
+>>>>>>> dac8696930e02caf5f5c1bae4678ded3d4deddc9
