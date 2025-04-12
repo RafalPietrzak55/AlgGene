9447fa44734b80f8053d942ce1040bcc481a4ab7
RafalPietrzak55 <115399790+RafalPietrzak55@users.noreply.github.com>
2025-04-12 09:17:27 +0200

    3

--- Program.cs
+++ Program.cs
@@ -1,24 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 class GeneticAlgorithm
 {
-    class Program
-    {
-        
-        
-      
-
-           Random random = new Random();
-
-            const int PopulationSize = 9;
-            const int ChromosomeBits = 3;
-            const int Iterations = 20;
-            const int Tournaments = 2;
-            const double MinValue = 0.0;
-            const double MaxValue = 100.0;    
     static Random random = new Random();
     const int PopulationSize = 9;
     const int ChromosomeBits = 3;
@@ -27,36 +12,10 @@ class GeneticAlgorithm
     const double MinValue = 0.0;
     const double MaxValue = 100.0;
 
-
-       
-
-        static void Main(string[] args)
-        
-        
-        {
-            List<int[]> population = InitializePopulation();
-
-            for (int iteration = 0; iteration <= Iterations; iteration++)
-
-            {
-                var fitnessValues = population.Select(DecodeAndEvaluate).ToList();
-
-                double bestFitness = fitnessValues.Max();
-                double averageFitness = fitnessValues.Average();
-
-                Console.WriteLine($"Iteracja {iteration}: Najlepsza = {bestFitness}, Srednia = {averageFitness}");
     static void Main(string[] args)
     {
         List<int[]> population = InitializePopulation();
 
-                if (iteration == Iterations) break;
-
-
-                List<int[]> newPopulation = new List<int[]>();
-
-
-
-            }
         for (int iteration = 0; iteration <= Iterations; iteration++)
         {
             var fitnessValues = population.Select(DecodeAndEvaluate).ToList();
@@ -82,62 +41,6 @@ class GeneticAlgorithm
         }
     }
 
-            static List<int> InitializePopulation()
-            {
-                List<int> population = new List<int>();
-                for (int i = 0; i < PopulationSize; i++)
-                {
-                    int[] chromosome = new int[ChromosomeBits * 2];
-                    for (int j = 0; j < chromosome.Length; j++)
-                        chromosome[j] = Random.Next(2);
-                    population.Add(chromosome);
-                }
-                return population;
-            }   
-           
-            
-
-
-
-
-            static double Decode(int[] bits)
-            {
-                int vaule = 0; 
-                for (int i = 0; i < bits.Length; i++)
-                {
-                    vaule += bits[i] * (1 << (bits.Length - i - 1));
-
-                    double range = MaxValue - MinValue; 
-                    return MinValue + vaule * range / (Math.Pow(2, ChromosomeBits) - 1);
-                }
-            }
-
-
-
-
-            static double FitnessFunction(double x1, double x2)
-            {
-                return Math.Sin(0.05 * x1) + Math.Sin(0.05 * x2) + 0.4 * Math.Sin(0.15 * x1) * Math.Sin(0.15 * x2);
-
-            }
-
-
-            static double DecodeAndEvaluate(int[] chromosome)
-            {
-                double x1 = Decode(chromosome.Take(ChromosomeBits).ToArray());
-                double x2 = Decode(chromosome.Skip(ChromosomeBits).Take(ChromosomeBits).ToArray()); 
-
-                return FitnessFunction(x1, x2);
-            }
-
-
-
-
-          
-
-
-
-
     static List<int[]> InitializePopulation()
     {
         List<int[]> population = new List<int[]>();
@@ -151,14 +54,6 @@ class GeneticAlgorithm
         return population;
     }
 
-        }
-
-
-
-            
-
-        }
-    }
     static double DecodeAndEvaluate(int[] chromosome)
     {
         double x1 = Decode(chromosome.Take(ChromosomeBits).ToArray());
@@ -192,7 +87,6 @@ class GeneticAlgorithm
         int bestIndex = tournamentIndices.OrderByDescending(i => fitnessValues[i]).First();
         return population[bestIndex];
     }
-
 
     static int[] Mutate(int[] chromosome)
     {
